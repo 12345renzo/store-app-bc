9da1aa79e25d44761f8ff4df1b9fad33d60f9126
12345renzo <renzogonzalesasencios@gmail.com>
2025-03-04 07:54:56 -0500

    pdf back4

--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -11,8 +11,8 @@ import { UsuarioService } from 'src/usuario/usuario.service';
 import { PedidoService } from 'src/pedido/pedido.service';
 import { DetallePedidoService } from 'src/detalle_pedido/detalle_pedido.service';
 import puppeteer from 'puppeteer-extra';
-import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import { EditPedidoDto } from './dto/editPedidoDto';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 @Injectable()
 export class StoreService {
